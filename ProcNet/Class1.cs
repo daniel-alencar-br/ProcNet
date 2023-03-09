1f412ffee2f8a35c4527a4d617455556fd6faace
daniel-alencar-br <danielalencar.br@gmail.com>
2023-03-08 21:55:35 -0300

    Adicionar arquivos de projeto.

---
+++ ProcNet/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
+
+namespace ProcNet
+{
+    public class Funcoes
+    {
+        [SqlProcedure]
+        public static void Dobrar(SqlInt32 iEnt, out SqlInt32 iSaida)
+        {
+            iSaida = iEnt * 2;
+        }
+    }
+
+}
